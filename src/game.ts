3254560bd71bb51c1b8f09503966320f30d4c8bf
Gabor Heja <gheja@u830g8.herd.git.stdio.hu>
2026-08-24 14:33:51 +0200

    Added puzzle menu, added some transitions

--- src/game.ts
+++ src/game.ts
@@ -24,11 +24,16 @@ class Game {
         if (puzzle) {
             var z = Math.min((window.innerWidth * 0.66) / puzzle.width, (window.innerHeight * 0.66) / puzzle.height)
             _game.gfx.setTarget(puzzle.left + puzzle.width/2, puzzle.top + puzzle.height/2, z)
-            window.setTimeout(this.puzzleEnterTimeout.bind(this), 1500)
+            window.setTimeout(this.transitionPuzzleEnter1.bind(this), 1000)
+            window.setTimeout(this.transitionPuzzleEnter2.bind(this), 2000)
         }
     }
 
-    puzzleEnterTimeout() {
+    transitionPuzzleEnter1() {
+        _puzzleMenuButton.style.opacity = "1"
+    }
+
+    transitionPuzzleEnter2() {
         _hint.style.opacity = "1"
     }
 
@@ -37,5 +42,8 @@ class Game {
         this.state = GameState.MainScreen
         this.gfx.setTarget(250, 250, 1.0)
         _hint.style.opacity = "0"
+        _puzzleMenuButton.style.opacity = "0"
+        _puzzleMenu.style.display = "none"
+        // _puzzleMenu.style.opacity = "0"
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,37 @@
 let _game: Game
 let _container: HTMLDivElement
 let _hint: HTMLDivElement
+let _puzzleMenuButton: HTMLDivElement
+let _puzzleMenu: HTMLDivElement // this is an overlay that is handled by changing "display", not "opacity"
 
 function init() {
     _container = document.getElementById("b") as HTMLDivElement
     _hint = document.getElementById("h") as HTMLDivElement
+    _puzzleMenuButton = document.getElementById("m") as HTMLDivElement
+    _puzzleMenu = document.getElementById("n") as HTMLDivElement
     
     _game = new Game()
     // game.init()
     _game.start()
     zoom_reset()
+
     // @ts-ignore - "possibly null"
-    document.getElementById("back").addEventListener("click", zoom_reset)
+    document.getElementById("leave").addEventListener("click", zoom_reset)
+    // @ts-ignore - "possibly null"
+    document.getElementById("back").addEventListener("click", hidePuzzleMenu)
+    _puzzleMenuButton.addEventListener("click", showPuzzleMenu)
 }
 
 window.addEventListener("load", init)
+
+function showPuzzleMenu() {
+    _puzzleMenu.style.display = "block"
+    // _puzzleMenu.style.opacity = "1"
+}
+
+function hidePuzzleMenu() {
+    _puzzleMenu.style.display = "none"
+}
 
 function zoom_here(obj: HTMLElement) {
     _game.gfx.setTarget(obj.offsetLeft + obj.offsetWidth/2, obj.offsetTop + obj.offsetHeight/2, 3.0)
