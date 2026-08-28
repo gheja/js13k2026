b9b862cd25ab4a066162a3f493c4dcdd2b4b869c
Gabor Heja <gheja@u830g8.herd.git.stdio.hu>
2026-08-28 14:23:10 +0200

    tools/puzzle_editor: current wip

--- tools/puzzle_editor/main.js
+++ tools/puzzle_editor/main.js
@@ -7,38 +7,108 @@ function init() {
     _list_items = document.getElementById("list_items")
 }
 
-function remove_item(obj) {
-    alert(obj)
+function remove_item(event) {
+    let row = event.target.parentNode
+    row.parentNode.removeChild(row)
+}
+
+function get_row_data(obj) {
+    if (obj) {
+        return [
+            parseInt(obj.children[0].value),
+            parseFloat(obj.children[1].value),
+            parseFloat(obj.children[2].value),
+            parseFloat(obj.children[3].value),
+            obj.children[4].checked,
+        ]
+    }
+    else {
+        return [ 1, 1, 1, 0, false ]
+    }
+}
+
+function get_puzzle_definition() {
+    let result = []
+
+    let b = _list_items.querySelectorAll(".list_row")
+    for (let c of b) {
+        result.push(get_row_data(b[b.length - 1]))
+    }
+
+    console.log(result)
 }
 
 function add_item() {
     let s = ""
 
-    s += `<div class="list_row" id="item_${_item_n}">`
-    s += `<input type="text" class="shape_index" value="1"/>`
-    s += `<input type="text" class="pos_x" value="1"/>`
-    s += `<input type="text" class="pos_y" value="1"/>`
-    s += `<input type="text" class="rotation" value="0"/>`
-    s += `<input type="checkbox" class="locked" value="1"/>`
-    s += `<button onclick="remove_item(this.parentNode); return false;">x</button>`
-    s += `</div>`
+    let div = document.createElement("div")
+    div.className = "list_row"
+    div.id = `item_${_item_n}`
 
-    _list_items.innerHTML += s
+    let defaults
 
-    let obj = document.getElementById(`item_${_item_n}`)
-    obj.children[0].addEventListener("keydown", on_key_down.bind(obj.children[0]))
-    obj.children[1].addEventListener("keydown", on_key_down.bind(obj.children[1]))
-    obj.children[2].addEventListener("keydown", on_key_down.bind(obj.children[2]))
-    obj.children[3].addEventListener("keydown", on_key_down.bind(obj.children[3]))
+    let b = _list_items.querySelectorAll(".list_row")
+    if (b.length > 0) {
+        defaults = get_row_data(b[b.length - 1])
+    }
+    else {
+        defaults = get_row_data(null)
+    }
 
-    obj.children[0].focus()
+    let a
+
+    a = document.createElement("input")
+    a.type = "text"
+    a.className = "shape_index"
+    a.value = defaults[0]
+    a.addEventListener("keydown", on_key_down.bind(a))
+    div.appendChild(a)
+
+    a = document.createElement("input")
+    a.type = "text"
+    a.className = "pos_x"
+    a.value = defaults[1]
+    a.addEventListener("keydown", on_key_down.bind(a))
+    div.appendChild(a)
+
+    a = document.createElement("input")
+    a.type = "text"
+    a.className = "pos_y"
+    a.value = defaults[2]
+    a.addEventListener("keydown", on_key_down.bind(a))
+    div.appendChild(a)
+
+    a = document.createElement("input")
+    a.type = "text"
+    a.className = "rotation"
+    a.value = defaults[3]
+    a.addEventListener("keydown", on_key_down.bind(a))
+    div.appendChild(a)
+
+    a = document.createElement("input")
+    a.type = "checkbox"
+    a.className = "lock"
+    a.value = "1"
+    a.checked = defaults[4]
+    a.addEventListener("keydown", on_key_down.bind(a))
+    div.appendChild(a)
+
+    a = document.createElement("button")
+    a.innerHTML = "x"
+    a.addEventListener("click", remove_item.bind(div))
+    div.appendChild(a)
+
+    _list_items.appendChild(div)
+
+    div.children[0].focus()
+    div.children[0].select()
 
     _item_n += 1
 
 }
 
 function on_key_down(event) {
-    console.log(event)
+    // console.log(event)
     var value = event.target.value
 
     if (event.keyCode == 38) { // up arrow
