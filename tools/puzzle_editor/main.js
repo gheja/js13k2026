5d3eccafd28c24a573ecf583976b7843f6c2186e
Gabor Heja <gheja@u830g8.herd.git.stdio.hu>
2026-08-28 15:55:30 +0200

    tools/puzzle_editor: current wip

--- tools/puzzle_editor/main.js
+++ tools/puzzle_editor/main.js
@@ -1,15 +1,134 @@
 "use strict"
 
+const LOCAL_STORAGE_KEY = "hg26:puzzle_editor:state"
+
 let _list_items
 let _item_n = 0
 
+let _editor_state = null
+
 function init() {
     _list_items = document.getElementById("list_items")
+    editor_load_state()
+    on_update()
 }
 
 function remove_item(event) {
     let row = event.target.parentNode
     row.parentNode.removeChild(row)
+    on_update()
+}
+
+function load_puzzle_from_state() {
+    sync_puzzle_selector()
+    // document.getElementById("puzzle_editor_id").value = _editor_state["active_editor_id"]
+
+    reset_active_puzzle_data()
+
+    for (var p of _editor_state["puzzles"]) {
+        if (p["editor_id"] == _editor_state["active_editor_id"])
+        {
+            document.getElementById("puzzle_uid").value = p["uid"]
+            document.getElementById("puzzle_description").value = p["description"]
+            for (var row of p.data) {
+                let div = add_item(false)
+
+                div.children[0].value = row[0] // shape index
+                div.children[1].value = row[1] // x
+                div.children[2].value = row[2] // y
+                div.children[3].value = row[3] // rot
+                div.children[4].checked = row[4] // lock
+            }
+        }
+    }
+
+    document.getElementById("active_editor_id").value = _editor_state["active_editor_id"]
+}
+
+function create_new_puzzle() {
+    // 1609113600
+    let new_id = "n" + (_editor_state.puzzles.length + 1)
+
+    console.log(`creating new puzzle: ${new_id}`)
+
+    _editor_state["puzzles"].push({"editor_id": new_id, "uid": new_id, "description": "new puzzle", "data": []})
+    _editor_state["active_editor_id"] = new_id
+}
+
+function sync_puzzle_selector() {
+    let option
+    let select = document.getElementById("puzzle_selector")
+    select.innerHTML = ""
+
+    for (let p of _editor_state["puzzles"]) {
+        option = document.createElement("option")
+        option.value = p["editor_id"]
+        option.innerHTML = `${p["editor_id"]}: ${p["description"]} (${p["uid"]})`
+        if (p["editor_id"] == _editor_state["active_editor_id"]) {
+            option.selected = true
+        }
+        select.appendChild(option)
+    }
+
+    // select.value = _editor_state["active_editor_id"]
+}
+
+function reset_active_puzzle_data() {
+    _list_items.innerHTML = ""
+}
+
+function editor_reset_all() {
+    _editor_state = {
+        "active_editor_id": null,
+        "puzzles": []
+    }
+
+    reset_active_puzzle_data()
+    create_new_puzzle()
+    load_puzzle_from_state()
+    // save_active_puzzle_to_state()
+    editor_save_state()
+    // update_puzzle_visuals()
+}
+
+function save_active_puzzle_to_state() {
+    for (var p of _editor_state["puzzles"]) {
+        if (p.editor_id == _editor_state["active_editor_id"])
+        {
+            p.uid = document.getElementById("puzzle_uid").value
+            p.description = document.getElementById("puzzle_description").value
+            p.data = get_active_puzzle_data()
+        }
+    }
+}
+
+function editor_save_state() {
+    console.log("saving editor state")
+    save_active_puzzle_to_state()
+
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(_editor_state))
+}
+
+function editor_load_state() {
+    console.log("loading editor state")
+
+    let a = window.localStorage.getItem(LOCAL_STORAGE_KEY)
+    _editor_state = null
+
+    if (a != null) {
+        console.log("state found, loading...")
+        try {
+            _editor_state = JSON.parse(a)
+        }
+        catch (e) {}
+    }
+
+    if (_editor_state == null)
+    {
+        console.log("no valid state found, resetting...")
+        editor_reset_all()
+    }
+    load_puzzle_from_state()
 }
 
 function get_row_data(obj) {
@@ -27,18 +146,18 @@ function get_row_data(obj) {
     }
 }
 
-function get_puzzle_definition() {
+function get_active_puzzle_data() {
     let result = []
 
     let b = _list_items.querySelectorAll(".list_row")
     for (let c of b) {
-        result.push(get_row_data(b[b.length - 1]))
+        result.push(get_row_data(c))
     }
 
-    console.log(result)
+    return result
 }
 
-function add_item() {
+function add_item(call_update_hook = true) {
     let s = ""
 
     let div = document.createElement("div")
@@ -105,22 +224,92 @@ function add_item() {
 
     _item_n += 1
 
+    if (call_update_hook) {
+        on_update()
+    }
+
+    return div
 }
 
 function on_key_down(event) {
-    // console.log(event)
-    var value = event.target.value
+    let count = 1
+    let step = 1
+    let direction
+
+    if (event.keyCode != 38 && event.keyCode != 40) {
+        return
+    }
 
     if (event.keyCode == 38) { // up arrow
-        event.target.value = parseFloat(event.target.value) + 1
-        event.target.select()
-        event.preventDefault()
+        direction = +1
     }
-    else if (event.keyCode == 40) { // down arrow
-        event.target.value = parseFloat(event.target.value) - 1
-        event.target.select()
-        event.preventDefault()
+    else { // down arrow
+        direction = -1
     }
+
+    if (event.target.className == "pos_x") {
+        let grid_value = document.getElementById("pos_grid").value.split(",")
+        step = parseFloat(grid_value[0])
+    }
+    else if (event.target.className == "pos_y") {
+        let grid_value = document.getElementById("pos_grid").value.split(",")
+        step = parseFloat(grid_value[1])
+    }
+    if (event.target.className == "rotation") {
+        let grid_value = document.getElementById("rot_grid").value.split(",")
+        step = parseFloat(grid_value[0])
+    }
+
+    event.target.value = Math.floor(parseFloat(event.target.value) / step + count * direction) * step
+
+    if (event.target.className == "rotation") {
+        // map it to 0..360
+        event.target.value = (parseFloat(event.target.value) % 360 + 360) % 360
+    }
+
+    event.target.select()
+    event.preventDefault()
+
+    on_update()
+}
+
+function on_update() {
+    editor_save_state()
+    document.getElementById("output").innerHTML = JSON.stringify(get_active_puzzle_data())
+}
+
+function puzzle_selector_load() {
+
+}
+
+function puzzle_selector_new() {
+    editor_save_state()
+    reset_active_puzzle_data()
+    create_new_puzzle()
+    load_puzzle_from_state()
+    sync_puzzle_selector()
+
+    on_update()
+}
+
+function on_puzzle_selector_change(obj) {
+    if (obj.value == _editor_state["active_editor_id"]) {
+        return
+    }
+    console.log(`selector value: ${obj.value}`)
+
+    editor_save_state()
+
+    _editor_state["active_editor_id"] = obj.value
+
+    load_puzzle_from_state()
+
+    on_update()
+}
+
+function on_save_button() {
+    // just trigger an update
+    on_update()
 }
 
 window.addEventListener("load", init)
