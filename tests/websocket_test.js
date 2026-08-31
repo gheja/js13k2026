2ae9e1c273761d9f699b803c7db8a79eac9a18a5
Gabor Heja <gheja@u830g8.herd.git.stdio.hu>
2026-08-31 23:10:52 +0200

    tests/WebSocket: now the leader sends out a status as soon as the client connects, every new client accepts the first state update from anyone, so bootstrapping can be done

--- tests/websocket_test.js
+++ tests/websocket_test.js
@@ -5,6 +5,7 @@ const WS_BASE = ""
 let ws
 let net_id
 let net_participants = []
+let bootstrapped = false
 let _state = {'peer_states': {}, "a": 42}
 let _my_state = {"num": 1234}
 let net_init_done = false
@@ -19,6 +20,7 @@ function net_disconnected() {
 
 function _log(s) {
     document.getElementById("messages").innerHTML += s + "<br/>"
+    document.getElementById("messages").scrollBy(0, 5000)
 }
 
 function net_hi_all() {
@@ -57,6 +59,7 @@ function net_send_state_if_leader() {
 
 function update_my_state() {
     _my_state.num = Math.round(Math.random() * 10000)
+    _state.peer_states[net_id] = JSON.parse(JSON.stringify(_my_state))
     net_send_my_state()
 }
 
@@ -89,17 +92,26 @@ function net_message(event) {
             // please don't have "|" in the JSON...
             let arr = msg.split('|')
             _log("[debug] " + JSON.stringify(arr))
+            // saying hi and adding to participants
             if (arr[0] == 'm') {
+                // this is needed for bootstrapping - the new client might become leader
+                net_send_state_if_leader()
+
                 if (net_participants.indexOf(arr[1]) == -1) {
                     net_participants.push(arr[1])
                     net_participants.sort()
                 }
             }
+            // state update (only trust the leader)
             else if (arr[0] == 's') {
-                // only accept from the leader
-                if (arr[1] == net_participants[0]) {
+                // only accept from the leader, or from anyone if it is the first update (because if we just became the leader we need to bootstrap)
+                if (arr[1] == net_participants[0] || !bootstrapped) {
                     try {
                         _state = JSON.parse(arr[2])
+                        if (!bootstrapped) {
+                            net_send_state_if_leader()
+                            bootstrapped = true
+                        }
                     }
                     catch (e) {}
                     _log(`* new state from leader: ${JSON.stringify(_state)}`)
@@ -108,6 +120,7 @@ function net_message(event) {
                     _log(`* refusing state update from ${arr[1]}`)
                 }
             }
+            // my_state update (per client, we trust everyone)
             else if (arr[0] == 't') {
                 try {
                     _state.peer_states[arr[1]] = JSON.parse(arr[2])
@@ -120,6 +133,11 @@ function net_message(event) {
     }
 
     document.getElementById("participants").innerHTML = net_participants.join("<br/>")
+    if (net_participants[0] == net_id) {
+        document.getElementById("participants").innerHTML += "<br/><b>I am the leader</b>"
+    }
+
+    document.getElementById("state").innerHTML = JSON.stringify(_state)
 }
 
 function init() {
