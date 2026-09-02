c22577052e9a3378b4641c776186808b8bf870ee
Gabor Heja <gheja@u830g8.herd.git.stdio.hu>
2026-09-03 00:01:20 +0200

    Leaderboard: unified the data into shared_public_state

--- src/leaderboard.ts
+++ src/leaderboard.ts
@@ -15,37 +15,38 @@ enum LbPSIndex {
 
 const LEADERBOARD_SIZE_PER_PUZZLE = 300
 
-let leaderboard_data = [
-    [101, 0, "n2", [111, 0, 0, 0, [[1,1]]]],
-    [101, 1, "n1", [333, 0, 0, 0, [[3,3],[3,3],[3,3]]]],
-    [102, 2, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-    [103, 3, "n1", [222, 0, 0, 0, [[2,2],[2,2]]]],
-    [104, 0, "n1", [222, 0, 0, 0, [[2,2],[2,2]]]],
-    [105, 1, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-    [106, 2, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-    [107, 4, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-    [108, 5, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-    [109, 5, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
-]
-
-let player_data = {
-    101: [101, "Rainbow 101"],
-    102: [102, "Dash 102"],
-    103: [103, "Aasdf 103"],
-    104: [104, "Basdf 104"],
-    105: [105, "fFAJja"],
-    106: [106, "Jhahah"],
-    107: [107, "IuqhgH"],
-    108: [108, "Bloopeee"],
-    109: [109, "Sugar"],
+let shared_public_state = {
+    "l": [ // leaderboard
+        [101, 0, "n2", [111, 0, 0, 0, [[1,1]]]],
+        [101, 1, "n1", [333, 0, 0, 0, [[3,3],[3,3],[3,3]]]],
+        [102, 2, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+        [103, 3, "n1", [222, 0, 0, 0, [[2,2],[2,2]]]],
+        [104, 0, "n1", [222, 0, 0, 0, [[2,2],[2,2]]]],
+        [105, 1, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+        [106, 2, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+        [107, 4, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+        [108, 5, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+        [109, 5, "n1", [444, 0, 0, 0, [[4,4],[4,4],[4,4],[4,4]]]],
+    ],
+    "p": { // player data
+        101: [101, "Rainbow 101"],
+        102: [102, "Dash 102"],
+        103: [103, "Aasdf 103"],
+        104: [104, "Basdf 104"],
+        105: [105, "fFAJja"],
+        106: [106, "Jhahah"],
+        107: [107, "IuqhgH"],
+        108: [108, "Bloopeee"],
+        109: [109, "Sugar"],
+    },
 }
 
 function leaderboard_process() {
     // this now only has the valid entries
-    console.log(JSON.stringify(leaderboard_data))
+    console.log(JSON.stringify(shared_public_state['l']))
 
     // sort them
-    leaderboard_data = leaderboard_data.sort((a: Array<any>, b: Array<any>) => {
+    shared_public_state['l'] = shared_public_state['l'].sort((a: Array<any>, b: Array<any>) => {
         // puzzle id ASC
         if (a[Lb1Index.PuzzleUid] != b[Lb1Index.PuzzleUid]) {
             return b[Lb1Index.PuzzleUid] > a[Lb1Index.PuzzleUid] ? -1 : +1
@@ -61,7 +62,7 @@ function leaderboard_process() {
 
     // limit all puzzle leaderboards to LEADERBOARD_SIZE_PER_PUZZLE entries
     let counts = {}
-    leaderboard_data = leaderboard_data.filter((a: Array<any>) => {
+    shared_public_state['l'] = shared_public_state['l'].filter((a: Array<any>) => {
         if (!(a[Lb1Index.PuzzleUid] in counts)) {
             counts[a[Lb1Index.PuzzleUid]] = 0
         }
@@ -73,25 +74,25 @@ function leaderboard_process() {
         return true
     })
 
-    console.log(JSON.stringify(leaderboard_data))
+    console.log(JSON.stringify(shared_public_state['l']))
 }
 
 function leaderboard_add(data: Array<any>) {
     let puzzle = new PuzzleBase()
 
-    // try {
+    try {
         let d = _knownPuzzles[data[Lb1Index.PuzzleUid]]
         puzzle.setup(d[0], d[1], d[2], data[Lb1Index.PlayerState])
         puzzle.shuffle()
         if (puzzle.isSolved()) {
             clog("puzzle solution validated")
-            leaderboard_data.push(data)
+            shared_public_state['l'].push(data)
         }
         else {
             clog("err: puzzle solution is invalid!")
         }
-    // }
-    // catch (e) {}
+    }
+    catch (e) {}
 }
 
 function leaderboard_get_as_html(puzzleUid: string) {
@@ -99,13 +100,13 @@ function leaderboard_get_as_html(puzzleUid: string) {
     let i = 0
     let lastStepCount = null
     let n
-    for (let d of leaderboard_data) {
+    for (let d of shared_public_state['l']) {
         if (d[Lb1Index.PuzzleUid] == puzzleUid) {
             i += 1
             n = "unknown"
-            if (d[Lb1Index.PlayerUid] in player_data) {
+            if (d[Lb1Index.PlayerUid] in shared_public_state['p']) {
                 // name index
-                n = player_data[d[Lb1Index.PlayerUid]][1]
+                n = shared_public_state['p'][d[Lb1Index.PlayerUid]][1]
             }
             result += "<code>" + (lastStepCount == d[Lb1Index.PlayerState][LbPSIndex.Swaps].length ? "--" : "#" + i) + "</code> " + REACTIONS[d[Lb1Index.PayerReactionIndex]] + " " + n + " (" + d[Lb1Index.PlayerState][LbPSIndex.Swaps].length + " steps)<br/>"
             lastStepCount = d[Lb1Index.PlayerState][LbPSIndex.Swaps].length
@@ -115,12 +116,12 @@ function leaderboard_get_as_html(puzzleUid: string) {
 }
 
 function leaderboard_add_player_info(playerUid: number, playerName: string) {
-    player_data[playerUid.toString()] = playerName
+    shared_public_state['p'][playerUid.toString()] = playerName
 }
 
 function leaderboard_update_profile(data: Array<any>) {
     // player uid must be the first item
-    player_data[data[0]] = data
+    shared_public_state['p'][data[0]] = data
 }
 
 function leaderboard_submit_result(data: Array<any>) {
